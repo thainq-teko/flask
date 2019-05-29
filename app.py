8ca6e81840e334c2b4bf072d9afd39a31698455e
thainq-teko <thai.nq@teko.vn>
2019-05-29 16:34:35 +0700

    validate exist of email and username

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+# -*- coding: utf-8 -*-
 import flask
-from flask import Flask,request, jsonify
+from flask import request, jsonify, json
 from flask_cors import CORS
 
 from flaskext.mysql import MySQL
@@ -13,7 +14,7 @@ CORS(app)
 bcrypt = Bcrypt(app)
 mysql = MySQL()
 
-#config mysql connection
+# config mysql connection
 app.config['MYSQL_DATABASE_USER'] = 'teko'
 app.config['MYSQL_DATABASE_PASSWORD'] = '1234'
 app.config['MYSQL_DATABASE_DB'] = 'flask_db'
@@ -23,7 +24,7 @@ mysql.init_app(app)
 conn = mysql.connect()
 pointer = conn.cursor()
 
-#config flask_mail
+# config flask_mail
 mail = Mail(app)
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
@@ -32,7 +33,7 @@ app.config['MAIL_PASSWORD'] = 'pa5512!@'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
-
+'''
 @app.route('/allUser', methods=['GET'])
 def all_user():
     pointer.execute("SELECT * FROM user")
@@ -44,29 +45,38 @@ def all_user():
             'name': i[1].encode('ascii','ignore') #convert unicode to string
         })
     return jsonify(result)
-
+'''
 @app.route('/register', methods=['POST'])
 def register():
     req = request.get_json()
-    name = req['username'].encode('ascii','ignore')
-    pw = req["password"].encode('ascii','ignore')
-    pw_hashed = bcrypt.generate_password_hash(req["password"]).decode('utf-8').encode('ascii','ignore')
-    email = req['email'].encode('ascii','ignore')
+    name = req["username"]
+    pw = req["password"]
+    pw_hashed = bcrypt.generate_password_hash(req["password"]).decode('utf-8').encode('ascii', 'ignore')
+    email = req['email'].encode('ascii', 'ignore')
+    # check isExist username & email
+    pointer.execute("select id from user where username = %s", str(name))
+    if len(pointer.fetchall()) > 0:
+        return jsonify({'code': 401, 'message': "account existed!"})
+    pointer.execute("select id from user where email = %s", str(email))
+    if len(pointer.fetchall()) > 0:
+        return jsonify({'code': 401, 'message': "email existed!"})
     # sql query
     record_for_inserting = (name, pw_hashed, email)
-    sql = """Insert into user (username, password, email) values (%s, %s, %s) """
-    #print(record_for_inserting)
+    sql = "Insert into user (username, password, email) values (%s, %s, %s)"
+    # print(record_for_inserting)
     pointer.execute(sql, record_for_inserting)
     conn.commit()
     print ("Record inserted successfully into db")
-    msg = Message('Your account info', sender = 'accrac016@gmail.com', recipients = ['thainq00@gmail.com'])
+    msg = Message('Your account info', sender='accrac016@gmail.com', recipients=['thainq00@gmail.com'])
     msg.body = "username: " + name + " pass: " + pw
     mail.send(msg)
-    return "1"
+
+    return jsonify({'code': 200, 'message': "create account successfully"})
+
 
 @app.route('/login', methods=['POST'])
 def login():
-    #get body info
+    # get body info
     req = request.get_json()
     name = req["username"]
     pw = req["password"]
@@ -75,10 +85,11 @@ def login():
     print(pointer.fetchall())
     return jsonify({'stt': 200, 'data': { "name" : name}})
 
-@app.route('/test')
+
+@app.route('/test', methods=['POST'])
 def test():
-    pw = 'asd'
-    pw_hash = bcrypt.generate_password_hash(pw).decode('utf-8')
-    print(pw_hash)
-    print(bcrypt.check_password_hash(pw_hash, pw))
+    req = request.get_json()
+    name = req["username"]
+    pointer.execute("select id from user where username = %s", str(name))
+    print(len(pointer.fetchall()))
     return jsonify({'stt' : 200, 'data': { "auth": 'true'}})

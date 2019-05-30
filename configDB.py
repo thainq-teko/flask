d1538f44fbe8192012ae240d83f7537233f82c48
thainq-teko <thai.nq@teko.vn>
2019-05-30 16:01:26 +0700

    add gitignore

--- configDB.py
+++
@@ -1,4 +0,0 @@
-name = "teko"
-passw = "1234"
-db = "flask_db"
-host = "localhost"
--- heroku.py
+++
@@ -1,4 +0,0 @@
-name = "bf6588dce14074"
-passw = "196959bf"
-db = "heroku_a24e410b25d00ab"
-host = "us-iron-auto-dca-02-a.cleardb.net"
